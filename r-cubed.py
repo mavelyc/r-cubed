85ab697c8714c75f348ccdd23c9b14e5224b90a6
mavelyc <cmavely@gmail.com>
2020-03-03 13:08:35 -0500

    python video feed with api call

---
+++ r-cubed.py
@@ -0,0 +1,50 @@
+import cv2
+import json
+from ibm_watson import VisualRecognitionV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+def main():
+    windowName = "Live Video Feed"
+    cv2.namedWindow(windowName)
+    cap = cv2.VideoCapture(0)
+    flag = 0
+    
+    if cap.isOpened():
+        ret, frame = cap.read()
+    else:
+        ret = False
+
+    while ret:
+
+        ret, frame = cap.read()
+
+        cv2.imwrite("frame.jpg", frame)     # save frame as JPEG file      
+        
+        #output = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        if flag == 0:
+            authenticator = IAMAuthenticator('{api-key}')
+            visual_recognition = VisualRecognitionV3(
+                version='2018-03-19',
+                authenticator=authenticator
+            )
+
+            visual_recognition.set_service_url('https://api.us-south.visual-recognition.watson.cloud.ibm.com/instances/d9ed5d97-8d20-4f96-8fc7-ad2b4e337061')
+
+            with open('./frame.jpg', 'rb') as image:
+                classes = visual_recognition.classify(
+                    images_file=image,
+                    threshold='0.6',
+                    ).get_result()
+                print(json.dumps(classes, indent=2))
+
+            cv2.imshow("Original Webcam Feed", frame)
+            
+        
+        if cv2.waitKey(1) == 27: # exit on ESC
+            break
+
+    cv2.destroyAllWindows()
+    cap.release()
+    
+if __name__ == "__main__":
+    main()
