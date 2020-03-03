cffa8a80ed5a92aa55651a0d10f8f4717e7c0d1b
Justeen-Randev <Justeen.Randev@ibm.com>
2020-03-03 14:50:02 -0500

    Menu screen

--- menu.py
+++ menu.py
@@ -1,5 +1,11 @@
 import tkinter
+from start import start_camera
 
 def make_menu():
     m=tkinter.Tk()
+    m.title('R-cubed')
+    startButton = tkinter.Button(m, text='Start', width=25, command=start_camera) 
+    button = tkinter.Button(m, text='Exit', width=25, command=m.destroy) 
+    button.pack() 
+    startButton.pack()
     m.mainloop()
--- r-cubed.py
+++ r-cubed.py
@@ -7,54 +7,7 @@ from menu import make_menu
 def main():
     make_menu()
 
-    windowName = "Live Video Feed"
-    cv2.namedWindow(windowName)
-    cap = cv2.VideoCapture(0)
-    count = 0
-    flag = 0
-
-    with open ("apikey", "r") as apikey_file:
-        api_key=apikey_file.readlines()[0]
     
-    if cap.isOpened():
-        ret, frame = cap.read()
-    else:
-        ret = False
-
-    while ret:
-
-        ret, frame = cap.read()
-
-        cv2.imwrite("frame.jpg", frame)     # save frame as JPEG file      
-        
-        #output = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        if flag == 0:
-            authenticator = IAMAuthenticator(api_key)
-            visual_recognition = VisualRecognitionV3(
-                version='2018-03-19',
-                authenticator=authenticator
-            )
-
-            visual_recognition.set_service_url('https://api.us-south.visual-recognition.watson.cloud.ibm.com/instances/d9ed5d97-8d20-4f96-8fc7-ad2b4e337061')
-
-            with open('./frame.jpg', 'rb') as image:
-                classes = visual_recognition.classify(
-                    images_file=image,
-                    threshold='0.6',
-                    ).get_result()
-                print(json.dumps(classes, indent=2))
-
-        
-        cv2.imshow("Original Webcam Feed", frame)
-        
-        if cv2.waitKey(1) == 27: # exit on ESC
-            break
-
-        flag += 1
-        flag = flag%30
-
-    cv2.destroyAllWindows()
-    cap.release()
     
 if __name__ == "__main__":
     main()
