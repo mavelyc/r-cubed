830e06670c44e7e4500696318024f209cddec007
DannyS03 <danny.salman@ryerson.ca>
2020-03-03 14:31:36 -0500

    gui specs test

--- menu.py
+++ menu.py
@@ -1,5 +1,24 @@
 import tkinter
+from tkinter import Button
+from ibm_watson.compare_comply_v1 import Label
 
 def make_menu():
     m=tkinter.Tk()
+
+    m.title("r-cubed")
+
+    lbl = Label(m, text="images")
+
+    lbl.grid(column=0, row=0)
+
+    btn = Button(m, text="Capture image")
+
+    def clicked():
+            
+        lbl.configure(text="Image clicked")
+
+    btn.grid(column=1, row=0)
+
     m.mainloop()
+
+
